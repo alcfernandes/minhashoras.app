453cbae6c9caca1725f1d226114d20902fe3a5e3
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-19 18:45:31 -0300

    refactor(SideBarMenu): Separate menu items into a different file
    This commit separates the definition of the menu items into a new file
    named 'menu-items.ts'.
    
    This improves the organization and maintainability of the code,
    making it easier to update the menu items in the future
    without touching the SideMenu component logic.

--- src/layouts/components/SideMenu.tsx
+++ src/layouts/components/SideMenu.tsx
@@ -1,61 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 
 import { Menu } from 'antd';
-import {
-  AccountAdminIcon,
-  ActivityIcon,
-  ClientsIcon,
-  DashboardIcon,
-  LogoutIcon,
-  ProfileIcon,
-  ProjectsIcon,
-  ReportsMenuIcon,
-  TimeSheetIcon,
-  TrackIcon,
-  UserMenuIcon,
-} from '@shared/components/app-icons';
 import { useAuth } from '@features/auth';
+import { menuItems } from './menu-itens';
 
 function SideMenu() {
   const { logout } = useAuth();
-
-  const items = [
-    { key: '/track', icon: TrackIcon, label: 'Track' },
-    { key: '/projects', icon: ProjectsIcon, label: 'Projects' },
-    {
-      key: 'reports-menu',
-      icon: ReportsMenuIcon,
-      label: 'Reports',
-      children: [
-        {
-          key: '/reports/dashboard',
-          icon: DashboardIcon,
-          label: 'Dashboard',
-        },
-        { key: '/reports/detailed', icon: ActivityIcon, label: 'Detailed' },
-        {
-          key: '/reports/timesheet',
-          icon: TimeSheetIcon,
-          label: 'Timesheet',
-        },
-      ],
-    },
-    { key: '/clients', icon: ClientsIcon, label: 'Clients' },
-    {
-      key: 'user-menu',
-      icon: UserMenuIcon,
-      label: 'Alessandro',
-      children: [
-        {
-          key: '/account-admin',
-          icon: AccountAdminIcon,
-          label: 'Settings',
-        },
-        { key: '/profile', icon: ProfileIcon, label: 'Profile' },
-        { key: '/sign-out', icon: LogoutIcon, label: 'Sign out' },
-      ],
-    },
-  ];
   const navigate = useNavigate();
 
   const handleMenuClick = ({ key }: { key: string }) => {
@@ -70,7 +20,7 @@ function SideMenu() {
     <Menu
       theme="dark"
       mode="inline"
-      items={items}
+      items={menuItems}
       onClick={handleMenuClick}
       defaultSelectedKeys={[window.location.pathname]}
     />
