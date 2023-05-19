a88e078e9b029841676b5b62f28c45fa8abe16bd
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-19 18:31:33 -0300

    refactor(SideBarMenu): Extract menu click handler
    This commit extracts the menu click handling into a separate function
    named `handleMenuClick` within the SideMenu component.
    
    This refactoring improves code readability and separation of concerns.

--- src/layouts/components/SideMenu.tsx
+++ src/layouts/components/SideMenu.tsx
@@ -58,18 +58,20 @@ function SideMenu() {
   ];
   const navigate = useNavigate();
 
+  const handleMenuClick = ({ key }: { key: string }) => {
+    if (key === '/sign-out') {
+      logout();
+    } else {
+      navigate(key);
+    }
+  };
+
   return (
     <Menu
       theme="dark"
       mode="inline"
       items={items}
-      onClick={({ key }) => {
-        if (key === '/sign-out') {
-          logout();
-        } else {
-          navigate(key);
-        }
-      }}
+      onClick={handleMenuClick}
       defaultSelectedKeys={[window.location.pathname]}
     />
   );
