069d461da0beaa5a7ca669ff8bd453ee8f4c0763
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-24 18:44:51 -0300

    build(dependencies): Add React Query

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { HashRouter } from 'react-router-dom';
 import './App.css';
 import { ConfigProvider, theme } from 'antd';
 import 'antd/dist/reset.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MainRoutes } from './routes';
 import { AuthProvider } from './features/auth/stores/AuthProvider';
 
 export function App() {
   return <MainRoutes />;
 }
+
+const queryClient = new QueryClient();
 
 export function WrappedApp() {
   return (
@@ -18,9 +21,11 @@ export function WrappedApp() {
           // algorithm: theme.defaultAlgorithm,
         }}
       >
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </QueryClientProvider>
       </ConfigProvider>
     </HashRouter>
   );
